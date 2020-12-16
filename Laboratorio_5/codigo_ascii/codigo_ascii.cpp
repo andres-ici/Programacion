e828922a89170802a6c576545fb5260b1cfac183
Andres <a.carrasco12@ufromail.cl>
2020-12-16 16:23:57 -0300

    Se agrega Lab 5

---
+++ Laboratorio_5/codigo_ascii/codigo_ascii.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char caracter;
+
+int main(){
+	
+	printf("Codigo ASCII de un caracter\n");
+	printf("Ingresar un solo caracter: \n");
+	
+	scanf("%c", &caracter);
+	
+	
+	printf("El codigo ASCII que corresponde al caracter que usted ingreso es: %i", caracter);
+	
+	}
